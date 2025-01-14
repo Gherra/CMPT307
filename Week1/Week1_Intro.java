168538d6c308107b7e3e97ad3ab7e141dd923061
Raman Kumar <raman16894@gmail.com>
2025-01-14 12:02:43 -0800

    Test code

---
+++ Week1/Week1_Intro.java
@@ -0,0 +1,10 @@
+package Week1;
+
+public class Week1_Intro {
+    public static void main(String[] args) {
+        int[] array  = {2, 4, 1, 5, 3, 6};
+        for (int j : array) {
+            System.out.println(j);
+        }
+    }
+}
